07ec33f646ade33d5c69dd7757f9bd14c5219d06
peppino55 <150270650+Peppino55@users.noreply.github.com>
2023-11-29 16:22:25 +0000

    Update 6 files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+/*import React, { useState } from "react";*/
+import styled from "styled-components";
+
+import Education from "./components/Education";
 
 //Dichiarazione Styled Components:
-const Div = styled.div`
-  margin: 100px auto;
-  padding: 50px;
-  background-color: #66A182;
-  border: 2px solid #2E4057;
-  max-width: 700px;
-  border-radius: 5px;
+const Header = styled.header`
+  background: brown;
+  height: auto;
+  font-size: 60px;
   display: flex;
-  flex-direction: column;
+  align-content: center;
 `;
 
-const H1 = styled.h1`
-  color: #2E4057;
-  margin: auto;
-  text-align: center;
-  font-size: 70px;
+const Contenitore = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Div = styled.div`
+  width: 35%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+const Img = styled.img`
+  max-width: -webkit-fill-available;
 `;
 
 const App = () => {
-
-  return(
-    <Div>
-      <H1>Hello world</H1>
-    </Div>
-  )
+  return (
+    <div>
+      <Header>Questo è un header</Header>
+      <Contenitore>
+        <Div>
+          <h1>sono nicola miragliotta</h1>
+          <h2>puzzo meno di Marco</h2>
+          <p>Non so cosa scrivere in questo spazio</p>
+        </Div>
+        <Div>
+          <Img src="https://www.disegnifacili.com/wp-content/uploads/2022/11/Spada-da-disegno-passo8.jpg" />
+          <p>Questa è una spada perché non sapevo che altra foto mettere</p>
+        </Div>
+      </Contenitore>
+      <Contenitore>
+        <p>
+          Ora scrivo un testo lungo.
+          aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        </p>
+      </Contenitore>
+      <Education />
+    </div>
+  );
 };
 
 export default App;
